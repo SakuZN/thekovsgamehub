bf41c4c1363c4eef091150234e7ee0ddc4e4dace
Zach Noche <106810417+SakuZN@users.noreply.github.com>
2023-09-17 09:45:16 +0800

    Optimize game image to reduce networkload

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -2,6 +2,7 @@ import {Game} from "../hooks/useGames.ts";
 import {Card, CardBody, Heading, HStack, Image} from "@chakra-ui/react";
 import PlatformIconlist from "./PlatformIconlist.tsx";
 import CriticScore from "./CriticScore.tsx";
+import cropImageURL from "../services/cropImageURL.tsx";
 
 interface GCProps {
     game : Game
@@ -11,7 +12,7 @@ interface GCProps {
 const GameCards = ( {game} : GCProps) => {
     return (
         <Card>
-            <Image src={game.background_image}/>
+            <Image src={cropImageURL(game.background_image)}/>
             <CardBody borderRadius={10} overflow='hidden'>
                 <Heading fontSize={'2xl'}>
                     {game.name}

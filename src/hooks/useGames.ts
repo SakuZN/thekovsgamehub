ba6a3ae82d96d0bdd79f397e43372292c2de7f52
Zach Noche <106810417+SakuZN@users.noreply.github.com>
2023-09-17 08:34:00 +0800

    Custom Hook to fetch games via rawg.io API

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react'
+import apiClient from "../services/api-client.ts";
+import {CanceledError} from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+const UseGames = () => {
+    const [games, setGames] = useState<Game[]>([
+
+    ])
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+
+        const controller = new AbortController()
+        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
+            .then(response => setGames(response.data.results))
+            .catch(error => {
+                if (error instanceof CanceledError) return;
+                setError(error.message)
+            })
+        return () => controller.abort()
+    }, []);
+
+    return {games, error}
+}
+export default UseGames
